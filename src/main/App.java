caf1485d588f94e09cfe57d91671805934559451
amjvidal <136771834+amjvidal@users.noreply.github.com>
2023-10-19 20:23:04 +0000

    Exercicios 1 e 2

--- src/main/App.java
+++ src/main/App.java
@@ -1,11 +1,22 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        for(int i = 0; i < arrayOrdenado.length; i++){
+            if(arrayOrdenado[i] == num){
+                return 1;
+            }
+        }
+        return 0;
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+        int memoria = -9999;
+        for(int i=0;i<arrayDesordenado.length;i++){
+            if(arrayDesordenado[i]>memoria){
+                memoria=arrayDesordenado[i];
+            }
+        }
+        return memoria;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
