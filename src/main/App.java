7708387cda7b556c8c32167d472089107bd1dc54
amjvidal <136771834+amjvidal@users.noreply.github.com>
2023-10-20 18:19:08 +0000

    Desafio 3

--- src/main/App.java
+++ src/main/App.java
@@ -19,7 +19,22 @@ public class App {
         return memoria;
     }
 
-    public static Integer parMaisProximo(Integer[][] pares){
-        return 1;
+    public static Double parMaisProximo(Integer[][] pares){
+        if (pares.length < 2) {
+            return null; 
+        }
+
+        double menorDistancia = Double.MAX_VALUE; 
+
+        for (int i = 0; i < pares.length; i++) {
+            for (int j = i + 1; j < pares.length; j++) {
+                double distancia = Math.sqrt(Math.pow(pares[i][0] - pares[j][0], 2) + Math.pow(pares[i][1] - pares[j][1], 2));
+                if (distancia < menorDistancia) {
+                    menorDistancia = distancia;
+                }
+            }
+        }
+
+        return menorDistancia;
     }
 }
